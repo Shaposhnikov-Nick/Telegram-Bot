b16d09ed9bbe8c54fb443fde4b8f2b8fec78c539
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-06-13 12:00:04 +0300

    [config] add security config

---
+++ config-server/src/main/kotlin/tgbot/configserver/config/SecurityConfig.kt
@@ -0,0 +1,29 @@
+package tgbot.configserver.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .formLogin { it.disable() }
+            .logout { it.disable() }
+            .cors { it.disable() }
+            .csrf { it.disable() }
+            .httpBasic { }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.anyRequest().authenticated()
+            }.build()
+    }
+
+}
