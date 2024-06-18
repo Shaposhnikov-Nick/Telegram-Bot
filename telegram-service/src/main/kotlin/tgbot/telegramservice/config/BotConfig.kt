4054bdab0ccb855008e84857d916c801de0bb673
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-06-18 14:30:07 +0300

    [telegram] fix bot config

--- telegram-service/src/main/kotlin/tgbot/telegramservice/config/BotConfig.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/config/BotConfig.kt
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
-import tgbot.telegramservice.service.IncomingMessageProcessing
+import tgbot.telegramservice.service.TelegramBot
 
 @Configuration
 class BotConfig {
 
     @Bean
-    fun telegramBotsApi(bot: IncomingMessageProcessing): TelegramBotsApi =
+    fun telegramBotsApi(bot: TelegramBot): TelegramBotsApi =
         TelegramBotsApi(DefaultBotSession::class.java).apply {
             registerBot(bot)
         }
