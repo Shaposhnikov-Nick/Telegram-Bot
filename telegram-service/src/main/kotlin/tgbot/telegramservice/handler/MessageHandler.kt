49af17c7c8133d368f191c40b78e03bf67cea13d
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-06-21 15:33:21 +0300

    [telegram] update message handler

--- telegram-service/src/main/kotlin/tgbot/telegramservice/handler/MessageHandler.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/handler/MessageHandler.kt
@@ -1,21 +1,32 @@
 package tgbot.telegramservice.handler
 
+import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Message
 import tgbot.telegramservice.handler.util.chatNotStartedMsg
 import tgbot.telegramservice.handler.util.getLastMainCommand
 import tgbot.telegramservice.handler.util.isChatStarted
+import tgbot.telegramservice.model.TranslateRequestEvent
+import tgbot.telegramservice.producer.Producer
+import tgbot.telegramservice.service.lastTranslateCommand
 
 
-object MessageHandler {
+@Component
+class MessageHandler(
+    val producers: Map<String, Producer>
+) {
     fun handle(msg: Message): SendMessage {
         val chatId = msg.chatId.toString()
         if (!isChatStarted(chatId)) return chatNotStartedMsg(chatId)
-        if (getLastMainCommand(chatId) == BotCommandHandler.MainCommand.TRANSLATE) {
-            // TODO("translate")
-            return SendMessage(chatId, "Translated")
+
+        val producer = when (getLastMainCommand(chatId)) {
+            BotCommandHandler.MainCommand.TRANSLATE -> producers["translate"]!!
+            BotCommandHandler.MainCommand.START -> TODO()
+            BotCommandHandler.MainCommand.STOP -> TODO()
+            null -> TODO()
         }
-        return SendMessage(chatId, "End")
+        producer.send(TranslateRequestEvent(chatId, msg.text, lastTranslateCommand[chatId]?.value!!.substring(1)))
+        return SendMessage(chatId, "Translated")
     }
 
 }
--- telegram-service/src/main/kotlin/tgbot/telegramservice/service/BotService.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/service/BotService.kt
@@ -1,5 +1,6 @@
 package tgbot.telegramservice.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -20,18 +21,21 @@ val chatStarted: ConcurrentHashMap<String, Boolean> = ConcurrentHashMap(16, 75.0
 
 @Component
 class TelegramBot(
-    private val botProperty: BotProperty
+    private val botProperty: BotProperty,
+    private val messageHandler: MessageHandler
 ) : TelegramLongPollingBot(botProperty.token) {
 
+    val log = LoggerFactory.getLogger(this::class.java)
     override fun getBotUsername(): String = botProperty.name
 
     override fun onUpdateReceived(update: Update) {
+        log.info("New Message ${update.message}")
         if (update.hasMessage()) {
             val msg = update.message
             if (msg.isCommand)
                 execute(BotCommandHandler.handle(msg))
             else
-                execute(MessageHandler.handle(msg))
+                execute(messageHandler.handle(msg))
         }
         if (update.hasCallbackQuery()) {
             CallbackHandler.handle(update)
