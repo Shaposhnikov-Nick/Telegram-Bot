f560bc46375c0314b8c560afbe0b02796af5e288
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-06-24 17:06:37 +0300

    [telegram] update message handler

--- telegram-service/src/main/kotlin/tgbot/telegramservice/handler/MessageHandler.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/handler/MessageHandler.kt
@@ -5,10 +5,11 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Message
 import tgbot.telegramservice.handler.util.chatNotStartedMsg
 import tgbot.telegramservice.handler.util.getLastMainCommand
+import tgbot.telegramservice.handler.util.getLastTranslateCommand
 import tgbot.telegramservice.handler.util.isChatStarted
+import tgbot.telegramservice.keyboard.translateResponseKeyboard
 import tgbot.telegramservice.model.TranslateRequestEvent
 import tgbot.telegramservice.producer.Producer
-import tgbot.telegramservice.service.lastTranslateCommand
 
 
 @Component
@@ -25,8 +26,8 @@ class MessageHandler(
             BotCommandHandler.MainCommand.STOP -> TODO()
             null -> TODO()
         }
-        producer.send(TranslateRequestEvent(chatId, msg.text, lastTranslateCommand[chatId]?.value!!.substring(1)))
-        return SendMessage(chatId, "Translated")
+        producer.send(TranslateRequestEvent(chatId, msg.text, getLastTranslateCommand(chatId)?.direction!!))
+        return translateResponseKeyboard(chatId, "Translated")
     }
 
 }
