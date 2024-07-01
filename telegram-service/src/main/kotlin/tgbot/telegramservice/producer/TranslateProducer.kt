9415f796724ea07e25019edaaf36fe36f1582a25
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-01 17:07:42 +0300

    [telegram] add translate producer

--- telegram-service/src/main/kotlin/tgbot/telegramservice/producer/TranslateProducer.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/producer/TranslateProducer.kt
@@ -1,24 +1,23 @@
 package tgbot.telegramservice.producer
 
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
+import tgbot.telegramservice.config.KafkaProperties
 import tgbot.telegramservice.model.TranslateRequestEvent
 
 
 @Service("translate")
 class TranslateProducer(
-    val kafkaTemplate: KafkaTemplate<String, TranslateRequestEvent>,
-    @Value("\${topic.translate.request.name}")
-    val translateRequestTopicName: String
+    val translateRequestKafkaTemplate: KafkaTemplate<String, TranslateRequestEvent>,
+    val kafkaProperties: KafkaProperties
 ) : Producer {
 
     val log = LoggerFactory.getLogger(this::class.java)
 
     override fun <T : BotEvent> send(event: T) {
         if (event !is TranslateRequestEvent) return
-        val result = kafkaTemplate.send(translateRequestTopicName, event.chatId, event).get()
+        val result = translateRequestKafkaTemplate.send(kafkaProperties.topics.translateRequest, event.chatId, event).get()
         log.info("Request: $event")
         log.info("Topic: ${result.recordMetadata.topic()}")
         log.info("Partition: ${result.recordMetadata.partition()}")
