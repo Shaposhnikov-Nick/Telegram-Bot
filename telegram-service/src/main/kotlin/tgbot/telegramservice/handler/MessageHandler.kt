5361f1aa0eaf7736bf21520c6c84264585177bf6
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-04 15:48:45 +0300

    [telegram] update message handler

--- telegram-service/src/main/kotlin/tgbot/telegramservice/handler/MessageHandler.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/handler/MessageHandler.kt
@@ -3,11 +3,8 @@ package tgbot.telegramservice.handler
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Message
-import tgbot.telegramservice.handler.util.chatNotStartedMsg
-import tgbot.telegramservice.handler.util.getLastMainCommand
-import tgbot.telegramservice.handler.util.getLastTranslateCommand
-import tgbot.telegramservice.handler.util.isChatStarted
-import tgbot.telegramservice.keyboard.translateResponseKeyboard
+import tgbot.telegramservice.handler.util.*
+import tgbot.telegramservice.keyboard.mainMenuKeyboard
 import tgbot.telegramservice.model.TranslateRequestEvent
 import tgbot.telegramservice.producer.Producer
 
@@ -18,16 +15,17 @@ class MessageHandler(
 ) {
     fun handle(msg: Message): SendMessage? {
         val chatId = msg.chatId.toString()
-//        if (!isChatStarted(chatId)) return chatNotStartedMsg(chatId)
+        if (!isChatStarted(chatId)) return chatNotStartedMsg(chatId)
 
-        val producer = when (getLastMainCommand(chatId)) {
-            BotCommandHandler.MainCommand.TRANSLATE -> producers["translate"]!!
-            BotCommandHandler.MainCommand.START -> TODO()
-            BotCommandHandler.MainCommand.STOP -> TODO()
-            null -> TODO()
+        val producer = when (getLastServiceCommand(chatId)) {
+            ServiceCommand.TRANSLATE -> producers["translate"]!!
+            else -> null
         }
-        producer.send(TranslateRequestEvent(chatId, msg.text, getLastTranslateCommand(chatId)?.direction!!))
-        return null
+
+        return if (producer != null) {
+            producer.send(TranslateRequestEvent(chatId, msg.text, getLastTranslateCommand(chatId)?.direction!!))
+        } else  mainMenuKeyboard(chatId)
+
     }
 
 }
