db6cd5f61604795d8c9d01b2c182b7981c76e8b0
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-04 15:47:36 +0300

    [telegram] update callback handler util

--- telegram-service/src/main/kotlin/tgbot/telegramservice/handler/util/CallbackHandlerUtil.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/handler/util/CallbackHandlerUtil.kt
@@ -1,18 +1,19 @@
 package tgbot.telegramservice.handler.util
 
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import tgbot.telegramservice.handler.BotCommandHandler
+import tgbot.telegramservice.handler.ServiceCommand
+import tgbot.telegramservice.handler.TranslateCommand
 import tgbot.telegramservice.keyboard.mainMenuKeyboard
 import tgbot.telegramservice.keyboard.translateKeyboard
 
 
-fun mainMenuCallbackHandler(chatId: String): SendMessage{
+fun mainMenuCallbackHandler(chatId: String): SendMessage {
     return mainMenuKeyboard(chatId)
 }
 
 fun translateCallbackHandler(chatId: String): SendMessage {
     return if (isChatStarted(chatId)) {
-        addLastMainCommand(chatId, BotCommandHandler.MainCommand.TRANSLATE)
+        addLastServiceCommand(chatId, ServiceCommand.TRANSLATE)
         translateKeyboard(chatId)
     } else chatNotStartedMsg(chatId)
 }
@@ -20,10 +21,10 @@ fun translateCallbackHandler(chatId: String): SendMessage {
 fun enRuTranslateCallbackHandler(chatId: String): SendMessage {
     return if (!isChatStarted(chatId)) {
         chatNotStartedMsg(chatId)
-    } else if (!isLastCommandTranslate(chatId)) {
+    } else if (!isLastServiceCommandTranslate(chatId)) {
         translatorNotSelectedMsg(chatId)
     } else {
-        addLastTranslateCommand(chatId, BotCommandHandler.TranslateCommand.EN_RU)
+        addLastTranslateCommand(chatId, TranslateCommand.EN_RU)
         SendMessage(chatId, "Введите слово или фразу на английском")
     }
 }
@@ -31,10 +32,10 @@ fun enRuTranslateCallbackHandler(chatId: String): SendMessage {
 fun ruEnTranslateCallbackHandler(chatId: String): SendMessage {
     return if (!isChatStarted(chatId)) {
         chatNotStartedMsg(chatId)
-    } else if (!isLastCommandTranslate(chatId)) {
+    } else if (!isLastServiceCommandTranslate(chatId)) {
         translatorNotSelectedMsg(chatId)
     } else {
-        addLastTranslateCommand(chatId, BotCommandHandler.TranslateCommand.RU_EN)
+        addLastTranslateCommand(chatId, TranslateCommand.RU_EN)
         SendMessage(chatId, "Введите слово или фразу по русски")
     }
 }
