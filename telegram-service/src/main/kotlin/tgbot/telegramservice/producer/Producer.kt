df9d5a30ef2d1bc583e7e4ee845055f110d75e77
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-04 15:49:01 +0300

    [telegram] update producer

--- telegram-service/src/main/kotlin/tgbot/telegramservice/producer/Producer.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/producer/Producer.kt
@@ -1,8 +1,10 @@
 package tgbot.telegramservice.producer
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+
 interface BotEvent
 
 interface Producer {
-    fun <T : BotEvent> send(event: T)
+    fun <T : BotEvent> send(event: T): SendMessage?
 }
 
--- telegram-service/src/main/kotlin/tgbot/telegramservice/producer/TranslateProducer.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/producer/TranslateProducer.kt
@@ -3,6 +3,7 @@ package tgbot.telegramservice.producer
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import tgbot.telegramservice.config.KafkaProperties
 import tgbot.telegramservice.model.TranslateRequestEvent
 
@@ -15,12 +16,13 @@ class TranslateProducer(
 
     val log = LoggerFactory.getLogger(this::class.java)
 
-    override fun <T : BotEvent> send(event: T) {
-        if (event !is TranslateRequestEvent) return
+    override fun <T : BotEvent> send(event: T): SendMessage? {
+        if (event !is TranslateRequestEvent) return null
         val result = translateRequestKafkaTemplate.send(kafkaProperties.topics.translateRequest, event.chatId, event).get()
         log.info("Request: $event")
         log.info("Topic: ${result.recordMetadata.topic()}")
         log.info("Partition: ${result.recordMetadata.partition()}")
         log.info("Offset: ${result.recordMetadata.offset()}")
+        return null
     }
 }
