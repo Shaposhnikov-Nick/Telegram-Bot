481dd5502eed8cd50a77af904031732f3215cce5
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-15 15:59:22 +0300

    [weather] init project

---
+++ weather-service/src/main/kotlin/tgbot/weatherservice/WeatherServiceApplication.kt
@@ -0,0 +1,11 @@
+package tgbot.weatherservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeatherServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<WeatherServiceApplication>(*args)
+}
---
+++ weather-service/src/test/kotlin/tgbot/weatherservice/WeatherServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package tgbot.weatherservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WeatherServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
