0e225be248c31323a94830501a0addb270b4429f
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-16 16:17:15 +0300

    [telegram] add weather consumer config

--- telegram-service/src/main/kotlin/tgbot/telegramservice/config/KafkaConsumerConfig.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/config/KafkaConsumerConfig.kt
@@ -9,6 +9,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import tgbot.telegramservice.model.TranslateResponseEvent
+import tgbot.telegramservice.model.WeatherResponseEvent
 
 
 @Configuration
@@ -18,16 +19,17 @@ class KafkaConsumerConfig(
 
     @Bean
     fun translateResponseListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, TranslateResponseEvent> {
+        val translateConsumerProps = kafkaProperties.consumers["translate-consumer"]
         val configProps = mapOf(
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaProperties.bootstrapServers,
-            ConsumerConfig.GROUP_ID_CONFIG to kafkaProperties.consumers.translateConsumer.groupId,
+            ConsumerConfig.GROUP_ID_CONFIG to translateConsumerProps?.groupId,
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
             ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
-            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to kafkaProperties.consumers.translateConsumer.autoOffsetReset,
-            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to kafkaProperties.consumers.translateConsumer.valueDeserializer,
-            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to kafkaProperties.consumers.translateConsumer.keyDeserializer,
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to translateConsumerProps?.autoOffsetReset,
+            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to translateConsumerProps?.valueDeserializer,
+            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to translateConsumerProps?.keyDeserializer,
             JsonDeserializer.TRUSTED_PACKAGES to "*",
-            JsonDeserializer.VALUE_DEFAULT_TYPE to kafkaProperties.consumers.translateConsumer.valueDefaultType
+            JsonDeserializer.VALUE_DEFAULT_TYPE to translateConsumerProps?.valueDefaultType
         )
 
         val consumerFactory = DefaultKafkaConsumerFactory(
@@ -35,6 +37,30 @@ class KafkaConsumerConfig(
         )
 
         val listenerContainerFactory = ConcurrentKafkaListenerContainerFactory<String, TranslateResponseEvent>()
+        listenerContainerFactory.consumerFactory = consumerFactory
+        return listenerContainerFactory
+    }
+
+    @Bean
+    fun weatherResponseListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, WeatherResponseEvent> {
+        val weatherConsumerProps = kafkaProperties.consumers["weather-consumer"]
+        val configProps = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaProperties.bootstrapServers,
+            ConsumerConfig.GROUP_ID_CONFIG to weatherConsumerProps?.groupId,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to weatherConsumerProps?.autoOffsetReset,
+            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to weatherConsumerProps?.valueDeserializer,
+            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to weatherConsumerProps?.keyDeserializer,
+            JsonDeserializer.TRUSTED_PACKAGES to "*",
+            JsonDeserializer.VALUE_DEFAULT_TYPE to weatherConsumerProps?.valueDefaultType
+        )
+
+        val consumerFactory = DefaultKafkaConsumerFactory(
+            configProps, StringDeserializer(), JsonDeserializer(WeatherResponseEvent::class.java)
+        )
+
+        val listenerContainerFactory = ConcurrentKafkaListenerContainerFactory<String, WeatherResponseEvent>()
         listenerContainerFactory.consumerFactory = consumerFactory
         return listenerContainerFactory
     }
