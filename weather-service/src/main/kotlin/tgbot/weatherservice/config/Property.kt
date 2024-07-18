734646cf90c142aa901e295fe92356c7d6dcc078
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-18 17:00:24 +0300

    [weather] add property

--- weather-service/src/main/kotlin/tgbot/weatherservice/config/Property.kt
+++ weather-service/src/main/kotlin/tgbot/weatherservice/config/Property.kt
@@ -7,5 +7,42 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @ConfigurationProperties(prefix = "web.client")
 class WebClientProperty {
-    var baseUrl: String = ""
+    lateinit var baseUrl: String
+    lateinit var token: String
+}
+
+@Configuration
+@ConfigurationProperties(prefix = "kafka")
+class KafkaProperties {
+    lateinit var bootstrapServers: String
+    lateinit var topics: Topics
+    lateinit var consumers: Consumers
+    lateinit var producers: Producers
+
+    class Topics {
+        lateinit var weatherRequest: String
+        lateinit var weatherResponse: String
+    }
+
+    class Consumers {
+        lateinit var weatherConsumer: Consumer
+
+        class Consumer {
+            lateinit var groupId: String
+            lateinit var autoOffsetReset: String
+            lateinit var keyDeserializer: String
+            lateinit var valueDeserializer: String
+            lateinit var valueDefaultType: String
+        }
+    }
+
+    class Producers {
+        lateinit var weatherProducer: Producer
+
+        class Producer {
+            lateinit var keySerializer: String
+            lateinit var valueSerializer: String
+        }
+    }
+
 }
