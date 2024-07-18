ace7af098962c3241bcf9f5f258fcc508417c0df
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-18 17:09:25 +0300

    [weather] add weather models

---
+++ weather-service/src/main/kotlin/tgbot/weatherservice/model/WeatherModel.kt
@@ -0,0 +1,69 @@
+package tgbot.weatherservice.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import tgbot.weatherservice.producer.BotEvent
+
+
+data class WeatherRequestEvent(
+    val chatId: String,
+    val query: String,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class WeatherResponse(
+    val name: String,
+    @JsonProperty("main")
+    val mainWeather: MainWeather,
+    val visibility: Int,
+    val wind: Wind?,
+    val clouds: Clouds?,
+    val rain: Rain?,
+    val snow: Snow?
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class MainWeather(
+    val humidity: Double,
+    val temp: Double,
+    @JsonProperty("feels_like")
+    val feelLike: Double
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Wind(
+    val speed: Double?,
+    val gust: Double?
+)
+
+data class Clouds(
+    val all: Int
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Rain(
+    val oneHour: Double?,
+    val threeHours: Double?
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Snow(
+    val oneHour: Double?,
+    val threeHours: Double?
+)
+
+data class WeatherResponseEvent(
+    val chatId: String,
+    val name: String,
+    val visibility: Int,
+    val humidity: Double,
+    val wind: Double?,
+    val windGust: Double?,
+    val temp: Double,
+    val feelLike: Double,
+    val clouds: Int?,
+    val rainOneHour: Double?,
+    val rainThreeHours: Double?,
+    val snowOneHour: Double?,
+    val snowThreeHours: Double?
+) : BotEvent
