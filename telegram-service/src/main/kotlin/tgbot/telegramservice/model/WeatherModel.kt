d65c0762ed1f71f8c4600801e30b5b965d33dc45
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-07-24 16:21:31 +0300

    [telegram] update toString for beautiful response

--- telegram-service/src/main/kotlin/tgbot/telegramservice/model/WeatherModel.kt
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/model/WeatherModel.kt
@@ -1,17 +1,19 @@
 package tgbot.telegramservice.model
 
+import tgbot.telegramservice.extensions.bold
 import tgbot.telegramservice.producer.BotEvent
 
 
 data class WeatherRequestEvent(
     val chatId: String,
     val query: String,
-): BotEvent
+) : BotEvent
 
 
 data class WeatherResponseEvent(
     val chatId: String,
     val name: String,
+    val description: String,
     val visibility: Int,
     val humidity: Double,
     val wind: Double?,
@@ -23,4 +25,42 @@ data class WeatherResponseEvent(
     val rainThreeHours: Double?,
     val snowOneHour: Double?,
     val snowThreeHours: Double?
-) : Event
+) : Event {
+    override fun toString(): String {
+        val weatherAsString = """
+        ${"Населенный пункт:".bold()} ${name.bold()}
+        Описание: $description
+        Температура: ${temp.toInt()} C, ощущается как: ${feelLike.toInt()} C
+        Видимость: $visibility м
+        Влажность: $humidity %
+        Ветер: $wind м/с, порывы: $windGust м/с
+        Облачность: $clouds %
+        """.trimIndent()
+
+        rainOneHour?.let {
+            weatherAsString.plus("""
+                Дождь 1ч: $it мм
+            """.trimIndent())
+        }
+
+        rainThreeHours?.let {
+            weatherAsString.plus("""
+                Дождь 3ч: $it мм
+            """.trimIndent())
+        }
+
+        snowOneHour?.let {
+            weatherAsString.plus("""
+                Снег 1ч: $it мм
+            """.trimIndent())
+        }
+
+        snowThreeHours?.let {
+            weatherAsString.plus("""
+                Снег 1ч: $it мм
+            """.trimIndent())
+        }
+
+        return weatherAsString
+    }
+}
