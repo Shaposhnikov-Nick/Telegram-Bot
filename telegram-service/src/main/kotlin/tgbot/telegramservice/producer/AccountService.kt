1e10e0b2d7194afdefb1fdd3e08d5cd73685a60d
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-08-08 12:25:53 +0300

    [telegram] add account service

---
+++ telegram-service/src/main/kotlin/tgbot/telegramservice/producer/AccountService.kt
@@ -0,0 +1,49 @@
+package tgbot.telegramservice.producer
+
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.RestClientException
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import tgbot.telegramservice.handler.AccountCommand
+import tgbot.telegramservice.model.AccountDto
+import tgbot.telegramservice.model.AccountRequest
+
+
+@Service("account")
+class AccountService(
+    val restClient: RestClient
+) : Producer {
+    override fun <T : BotEvent> send(event: T): SendMessage? {
+        if (event !is AccountRequest) return null
+
+        val response = when (event.lastAccountCommand) {
+            AccountCommand.GET -> {
+                try {
+                    restClient
+                        .get()
+                        .uri("/account/${event.chatId}")
+                        .retrieve()
+                        .body(AccountDto::class.java)
+                } catch (e: RestClientException) {
+                    AccountDto(event.chatId, "Нет данных")
+                }
+            }
+
+            AccountCommand.SAVE -> {
+                val request = AccountDto(event.chatId, event.request!!.name, event.request.email, event.request.about)
+                restClient
+                    .post()
+                    .body(request)
+                    .retrieve()
+                    .body(AccountDto::class.java)
+            }
+        }
+
+        return SendMessage.builder()
+            .chatId(event.chatId)
+            .text(response!!.getMessageBody())
+            .disableWebPagePreview(false)
+            .parseMode("html")
+            .build()
+    }
+}
