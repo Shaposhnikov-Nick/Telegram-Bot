07ecf32a91a5ae1e8d90a89c77b5788fe5879dd1
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-08-15 15:51:54 +0300

    [account] add repository test

---
+++ account-service/src/test/kotlin/tgbot/accountservice/repository/AccountRepositoryTest.kt
@@ -0,0 +1,40 @@
+package tgbot.accountservice.repository
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import tgbot.accountservice.BaseTest
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class AccountRepositoryTest: BaseTest() {
+
+    @Test
+    fun `Save account`() {
+        val saved = accountRepository.save(account)
+
+        assertThat(saved)
+            .isNotNull()
+            .hasFieldOrProperty("id").isNotNull()
+            .hasFieldOrPropertyWithValue("chatId", account.chatId)
+            .hasFieldOrPropertyWithValue("name", account.name)
+            .hasFieldOrPropertyWithValue("email", account.email)
+            .hasFieldOrPropertyWithValue("about", account.about)
+    }
+
+    @Test
+    fun `Get account by chatId`() {
+        accountRepository.save(account)
+
+        val saved = accountRepository.findAccountByChatId(chatId)
+
+        assertThat(saved)
+            .isNotNull()
+            .hasFieldOrProperty("id").isNotNull()
+            .hasFieldOrPropertyWithValue("chatId", account.chatId)
+            .hasFieldOrPropertyWithValue("name", account.name)
+            .hasFieldOrPropertyWithValue("email", account.email)
+            .hasFieldOrPropertyWithValue("about", account.about)
+    }
+}
