cfca07d7de27fbc4890b7b57ea8668fdcbcf59db
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-08-15 15:50:49 +0300

    [account] add testcontainers configuration

--- account-service/src/test/kotlin/tgbot/accountservice/AccountServiceApplicationTests.kt
+++ account-service/src/test/kotlin/tgbot/accountservice/AccountServiceApplicationTests.kt
@@ -2,9 +2,7 @@ package tgbot.accountservice
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 
-@Import(TestcontainersConfiguration::class)
 @SpringBootTest
 class AccountServiceApplicationTests {
 
--- account-service/src/test/kotlin/tgbot/accountservice/TestAccountServiceApplication.kt
+++ account-service/src/test/kotlin/tgbot/accountservice/TestAccountServiceApplication.kt
@@ -1,9 +1,8 @@
 package tgbot.accountservice
 
 import org.springframework.boot.fromApplication
-import org.springframework.boot.with
 
 
 fun main(args: Array<String>) {
-	fromApplication<AccountServiceApplication>().with(TestcontainersConfiguration::class).run(*args)
+	fromApplication<AccountServiceApplication>().run(*args)
 }
