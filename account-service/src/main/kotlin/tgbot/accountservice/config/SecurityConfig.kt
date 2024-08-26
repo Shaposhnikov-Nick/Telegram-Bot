fced591c5d5ba23667426357e55088abd3ec19ec
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-08-26 16:28:12 +0300

    [account] add basic authentication

---
+++ account-service/src/main/kotlin/tgbot/accountservice/config/SecurityConfig.kt
@@ -0,0 +1,30 @@
+package tgbot.accountservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .formLogin { it.disable() }
+            .logout { it.disable() }
+            .csrf { it.disable() }
+            .cors { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .httpBasic { }
+            .authorizeHttpRequests {
+                it.anyRequest().authenticated()
+            }
+            .build()
+    }
+
+}
